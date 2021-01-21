12d02e54f76ec75d569458602bf7afe77cf657e2
Matteo <m2lovric@gmail.com>
2021-01-22 00:14:47 +0100

    Sign in and up pages init

---
+++ src/pages/signin.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const SignIn = () => {
+  return (
+    <Layout>
+      <form action="">
+        <input type="email" name="" id="" />
+        <input type="password" name="" id="" />
+      </form>
+    </Layout>
+  )
+}
+
+export default SignIn;
---
+++ src/pages/signup.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Layout from '../components/layout';
+import { createUser } from '../modules/Authentication/auth';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await createUser(email, password);
+    setEmail('');
+    setPassword('');
+  }
+
+  return (
+    <Layout>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-mail: </label>
+        <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">SignUp</button>
+      </form>
+    </Layout>
+  )
+}
+
+export default SignUp;

d30de0ab706a4f88d8c8b5624c2b121dd2278f96
Matteo <m2lovric@gmail.com>
2021-01-22 00:12:04 +0100

    Add redux, sass and ts plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,38 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-react-redux`,
+      options: {
+        // [required] - path to your createStore module
+        pathToCreateStoreModule: './src/state/createStore',
+        // [optional] - options passed to `serialize-javascript`
+        // info: https://github.com/yahoo/serialize-javascript#options
+        // will be merged with these defaults:
+        serialize: {
+          space: 0,
+          // if `isJSON` is set to `false`, `eval` is used to deserialize redux state,
+          // otherwise `JSON.parse` is used
+          isJSON: true,
+          unsafe: false,
+          ignoreFunction: true,
+        },
+        // [optional] - if true will clean up after itself on the client, default:
+        cleanupOnClient: true,
+        // [optional] - name of key on `window` where serialized state will be stored, default:
+        windowKey: '__PRELOADED_STATE__',
+      }
+    },
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
+    'gatsby-plugin-typescript',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {

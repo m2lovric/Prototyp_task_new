d8368d38d911857a1473158b9e7d3251cb475cda
m2lovric <m2lovric@gmail.com>
2021-01-24 21:10:36 +0100

    Update signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from '../components/layout';
+import { signIn } from '../modules/Authentication/auth';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    signIn(email, password);
+    setEmail('');
+    setPassword('');
+  }
   return (
     <Layout>
-      <form action="">
-        <input type="email" name="" id="" />
-        <input type="password" name="" id="" />
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Sign In</button>
       </form>
     </Layout>
   )

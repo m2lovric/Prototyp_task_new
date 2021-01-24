a95bf610ed4ab3dfe1faf698513f19729dbd136d
m2lovric <m2lovric@gmail.com>
2021-01-24 21:09:37 +0100

    Update layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,44 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from 'gatsby';
-import "./layout.scss"
+import { navigate } from "@reach/router";
+import "./layout.scss";
+import { auth } from '../modules/Firebase/firebase';
+import { signOut } from '../modules/Authentication/auth';
 
 const Layout = ({ children }: any) => {
+  type UserState = Object | undefined;
+  const [user, setUser] = useState<UserState>(undefined)
+
+  auth.onAuthStateChanged(
+    user => {
+      if (user) {
+        console.log(user.uid, user.email)
+        setUser(user)
+      } else {
+        setUser(undefined);
+      }
+    }
+  )
+
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    signOut();
+    navigate('/');
+  }
+
   return (
     <>
       <header>
         <Link to="/">Home</Link>
         <Link to="/signup">Sign Up</Link>
         <Link to="/signin">Sign In</Link>
+        {user ? (
+          <div>
+            <Link to="/profile">Profile</Link> 
+            <button onClick={handleSignOut}>Sign Out</button>
+          </div>
+        ): '' }
+        
       </header>
       <main>{children}</main>
 

3115c0b6a7e466eab8caef81db5111e8ee623bad
m2lovric <m2lovric@gmail.com>
2021-01-24 21:10:11 +0100

    Test redux dispatch useSelector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Link } from 'gatsby';
-
+import React, {useEffect} from 'react';
 import Layout from "../components/layout"
+import { useSelector, useDispatch } from 'react-redux'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state)
+
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>{counter}</p>
+      <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
+    </Layout>
+  )
+}
 
 export default IndexPage

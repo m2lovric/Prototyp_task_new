9e03e78b40eddd48bb08607a3917076bbe95ed7b
m2lovric <m2lovric@gmail.com>
2021-01-24 21:09:46 +0100

    Add new functions

--- src/modules/Authentication/auth.ts
+++ src/modules/Authentication/auth.ts
@@ -1,13 +1,11 @@
 import { auth } from '../Firebase/firebase';
 
-export const createUser = (email: string, password: string) => {
-  auth.createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user)
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-}
+export const createUser = (email: string, password: string) => auth.createUserWithEmailAndPassword(email, password);
+
+export const signOut = () => auth.signOut().then(() => {
+    console.log('Signed out');
+  }).catch((error) => {
+    console.log('err', error);
+  });
+
+export const signIn = (email: string, password: string) => auth.signInWithEmailAndPassword(email, password);

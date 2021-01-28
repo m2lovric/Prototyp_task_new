fb881284dd2fa4d62562b5b964e83582ebae792c
Matteo <m2lovric@gmail.com>
2021-01-28 21:37:09 +0100

    Import reducers and store

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,12 +1,19 @@
 import React from "react"
 import { Provider } from "react-redux"
-import { createStore } from 'redux';
+import { createStore} from 'redux';
+import { combineReducers } from 'redux';
 
-import counterReducer from './src/store/counterReducer';
+import counterReducer from './src/Store/counterReducer';
+import toDoReducer from './src/Store/toDoReducer';
+import userReducer from './src/Store/userReducer';
 
 const extension = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-export const store = createStore(counterReducer, extension);
+export const store = createStore(combineReducers({
+  user: userReducer,
+  list: toDoReducer,
+  counter: counterReducer
+}), extension);
 
 export default ({ element }) => {
   return <Provider store={store}>{element}</Provider>
